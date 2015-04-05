53f843478b757ae00873383a719e65f3819ca58a
Jochem Verburg <jochemverburg@gmail.com>
2015-04-05 15:51:18 +0200

    Added code to find nicknames using nl-dbpedia

--- Wimbledonizer/src/DBPediaLink/SemanticWebAnalyzerInterface.java
+++ Wimbledonizer/src/DBPediaLink/SemanticWebAnalyzerInterface.java
@@ -4,11 +4,13 @@ import java.text.Normalizer;
 
 public interface SemanticWebAnalyzerInterface {
 	public static final String DBPEDIA_SPARQL = "http://dbpedia.org/sparql";
+	public static final String DBPEDIANL_SPARQL = "http://nl.dbpedia.org/sparql";
 	public static final String QUERY_PREFIXES = "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> "
 						    		+ "PREFIX dbpprop: <http://dbpedia.org/property/> "
 						    		+ "PREFIX dbres: <http://dbpedia.org/resource/> "
 						    		+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-						    		+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>";
+						    		+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
+						    		+ "PREFIX prop-nl: <http://nl.dbpedia.org/property/>";
 	
 	/**
 	 * @param name The name of the person
